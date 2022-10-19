7a90169f4600cca3da7f2349251c662a9ace550f
Diomar Villarroel <diomarfvr@gmail.com>
2022-10-18 23:56:56 -0400

    primer commit, proyecto finalizado

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button
+        type="button"
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="$emit('fn')"
+    >{{operator}}</button>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    operator: String
+});
+
+console.log(props);
+
+</script>
